acc62ca1cce3fba4c1d670a4f8862291e01b5d32
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-06 16:37:26 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
-def reformat_languages(languages)
+
   # your code here
-end
+  def reformat_languages(languages)
+  new_hash = {} 
+  
+  languages.each do |type, hash|
+    
+    hash.each do |name, attributes|
+      
+      new_hash[name] ||= attributes
+      
+      new_hash[name][:style] ||= []
+      
+      new_hash[name][:style] << type 
+    end 
+  end 
+  new_hash
+end 
+
